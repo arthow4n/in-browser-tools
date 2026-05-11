636c792e03ec3f2b0f7083e76e78b883aa6bd1b6
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2026-05-11 08:02:16 +0200

    Delete verify_ui.cjs

--- verify_ui.cjs
+++
@@ -1,9 +0,0 @@
-const { chromium } = require('playwright');
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
-  await page.goto('http://localhost:3000/text-adventure-writer');
-  await page.waitForSelector('#regenerate-last-btn');
-  console.log('Button found!');
-  await browser.close();
-})();
